58464ef95d8d31a147f6f38c428de2aae1ab841b
Valentin Tureac <valentin.tureac@kalon.ro>
2015-09-26 12:43:14 +0300

    Vehicle props

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,31 @@ require('./style.scss')
 import React from 'react'
 import { Vehicle } from './vehicle.jsx'
 
+var vehicleProps = {
+  "wheels" : {
+    "left": 10,
+    "right": 20
+  },
+
+  "axis": [{
+    "alpha" : 30,
+    "length": 20,
+  },
+  {
+    "alpha" : 10,
+    "length": 39,
+  },
+  {
+    "alpha" : 20,
+    "length": 3,
+  }
+  ]
+}
+
 window.addEventListener('load', () => {
   React.render(
-    <Vehicle />,
+    <Vehicle geometry={vehicleProps} />,
+
     document.querySelector('body')
-  )
+    )
 })
--- src/vehicle.jsx
+++ src/vehicle.jsx
@@ -5,10 +5,18 @@ export class Vehicle extends React.Component {
   render() {
     return (
       <div className="Vehicle">
-        <svg className="Vehicle-svg" width="100" height="40">
+        <svg className="Vehicle-svg" width="120" height="60">
+          <g className="Vehicle-body">
+            <polyline points={"15 45, 60 30, 105 45,"+ this.props.geometry.axis.map((point) =>{
+              let x = Math.ceil(point.length*Math.cos(point.alpha) + 60)
+              let y = Math.ceil(point.length*Math.sin(point.alpha) + 30)
+
+              return " "+x+" "+y+"";
+            })} />
+          </g>
           <g className="Vehicle-wheels">
-            <circle cx="10" cy="10" r="10" />
-            <circle cx="80" cy="10" r="10" />
+            <circle cx="15" cy="45" r={this.props.geometry.wheels.left} />
+            <circle cx="105" cy="45" r={this.props.geometry.wheels.right} />
           </g>
         </svg>
       </div>
