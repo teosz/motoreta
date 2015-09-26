a7dcc583827b7b071f3e181d2c52566737c6cfbf
Valentin Tureac <valentin.tureac@kalon.ro>
2015-09-26 17:29:55 +0300

    Add clouds

--- src/race-view.jsx
+++ src/race-view.jsx
@@ -38,6 +38,13 @@ export class RaceView extends React.Component {
           <Terrain geometry={model.terrain} />
           {model.vehicles.map((v) => <div style={{transform: "translate("+v.x+"px,"+v.y+"px)"}} className="Vehicle-container"><Vehicle geometry={v} /></div>)}
         </div>
+        <div id="clouds">
+          <div className="cloud x1"></div>
+          <div className="cloud x2"></div>
+          <div className="cloud x3"></div>
+          <div className="cloud x4"></div>
+          <div className="cloud x5"></div>
+        </div>
       </div>
     )
   }
